080811f61f1a8a729f6940a703e55eaad32b0658
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-01-29 01:12:15 +0200

    Install @clerk/nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ClerkProvider } from '@clerk/nextjs';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
