e282651649aeb8fd1cf0fe0b5102a58b864f33a2
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-02-02 16:16:04 +0200

    Update `@clerk/nextjs` package

--- pages/ssr-enabled.tsx
+++ pages/ssr-enabled.tsx
@@ -1,10 +1,10 @@
-import type { InferGetServerSidePropsType, NextPage } from 'next';
-import { useUser, SignOutButton, SignInButton } from '@clerk/nextjs';
+import type { InferGetServerSidePropsType } from 'next';
+import { SignInButton, SignOutButton, useUser } from '@clerk/nextjs';
 import { withServerSideAuth } from '@clerk/nextjs/ssr';
 
 export const getServerSideProps = withServerSideAuth(async context => {
   const { sessionId, userId, getToken } = context.auth;
-  console.log('Available during SSR', sessionId, userId, await getToken());
+  console.log('Available during SSR:', sessionId, userId, await getToken());
   return {
     props: {
       message: 'You can return any custom props as usual, and of course, everything is properly typed',
@@ -13,8 +13,8 @@ export const getServerSideProps = withServerSideAuth(async context => {
 });
 
 const SsrEnabled = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { isLoading, isSignedIn, user } = useUser();
-  console.log('useUser(): ', { isLoading, isSignedIn, user });
+  const { isLoaded, isSignedIn, user } = useUser();
+  console.log('useUser(): ', { isLoaded, isSignedIn, user });
 
   return (
     <div>
@@ -22,13 +22,18 @@ const SsrEnabled = (props: InferGetServerSidePropsType<typeof getServerSideProps
       {!user && <SignInButton mode='modal' redirectUrl={'./'} />}
       <h1>useUser state</h1>
       <pre>
-        isLoading: {JSON.stringify(isLoading)} <br />
+        isLoaded: {JSON.stringify(isLoaded)} <br />
         isSignedIn: {JSON.stringify(isSignedIn)} <br />
         user: {JSON.stringify(user)} <br />
       </pre>
       <hr />
       <p>Clerk integrates with NextJs SSR seamlessly: </p>
-      <pre>{props.message}</pre>
+      <pre>
+        props.message
+        <br />
+        {'> '}
+        {props.message}
+      </pre>
     </div>
   );
 };
--- pages/ssr-preload.tsx
+++ pages/ssr-preload.tsx
@@ -1,43 +1,58 @@
-import type { InferGetServerSidePropsType, NextPage } from 'next';
-import { useUser, SignOutButton, SignInButton, useSession } from '@clerk/nextjs';
+import type { InferGetServerSidePropsType } from 'next';
+import { SignInButton, SignOutButton, useSession, useUser } from '@clerk/nextjs';
 import { withServerSideAuth } from '@clerk/nextjs/ssr';
 
 export const getServerSideProps = withServerSideAuth(
   async context => {
     const { sessionId, userId, getToken } = context.auth;
     const { user, session } = context;
-    console.log('Available during SSR', sessionId, userId, await getToken());
-    console.log('Available during SSR', user, session);
+    console.log('Available during SSR:', sessionId, userId, await getToken());
+    console.log('Available during SSR:', user, session);
   },
   { loadUser: true, loadSession: true },
 );
 
 const SsrPreload = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { isLoading, isSignedIn, user } = useUser();
-  const { session } = useSession();
+  return (
+    <div>
+      <PreloadedUser />
+      <PreloadedSession />
+    </div>
+  );
+};
 
-  console.log('useUser(): ', { isLoading, isSignedIn, user });
-  console.log('useSession(): ', { isLoading, isSignedIn, session });
+function PreloadedUser() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  console.log('useUser(): ', { isLoaded, isSignedIn, user });
 
   return (
     <div>
+      <h1>useUser state</h1>
       {user && <SignOutButton />}
       {!user && <SignInButton mode='modal' redirectUrl={'./'} />}
-      <h1>useUser state</h1>
       <pre>
-        isLoading: {JSON.stringify(isLoading)} <br />
+        isLoaded: {JSON.stringify(isLoaded)} <br />
         isSignedIn: {JSON.stringify(isSignedIn)} <br />
         user: {JSON.stringify(user)} <br />
       </pre>
-      <hr />
+    </div>
+  );
+}
+
+function PreloadedSession() {
+  const { isLoaded, isSignedIn, session } = useSession();
+  console.log('useSession(): ', { isLoaded, isSignedIn, session });
+
+  return (
+    <div>
       <h1>useSession state</h1>
       <pre>
-        isLoading: {JSON.stringify(isLoading)} <br />
+        isLoaded: {JSON.stringify(isLoaded)} <br />
         isSignedIn: {JSON.stringify(isSignedIn)} <br />
         session: {JSON.stringify(session)} <br />
       </pre>
     </div>
   );
-};
+}
 
 export default SsrPreload;
